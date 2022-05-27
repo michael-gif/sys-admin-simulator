9f76ea3978fe2e1f9aa563f0663b1d9a3ba42e21
John <mwxfranco@gmail.com>
2022-05-27 11:35:34 +0100

    added scroll functionality to console

--- main.py
+++ main.py
@@ -18,14 +18,29 @@ def clear():
 
 
 history = []
+visible_history = []
 command_history = []
+x, y = screen.get_size()
+possible_lines = ((y - 5) // font_size) - 1
+mouse_wheel_offset = 0
 def render_history():
-    for i in range(len(history)):
-        text = font.render(str(history[i]), False, (255, 255, 255))
+    global visible_history, mouse_wheel_offset
+    if mouse_wheel_offset < 0:
+        mouse_wheel_offset = 0
+    if mouse_wheel_offset > len(history) - possible_lines:
+        mouse_wheel_offset = len(history) - possible_lines
+    if len(history) > possible_lines:
+        visible_history = history[possible_lines * -1 - mouse_wheel_offset:len(history) - mouse_wheel_offset + 1]
+    else:
+        visible_history = history
+    for i in range(len(visible_history)):
+        text = font.render(str(visible_history[i]), False, (255, 255, 255))
         screen.blit(text, (5, 5 + (i * font_size)))
+
 
 def console(message):
     history.append(message)
+
 
 prev_time = 0
 cursor_delay = 0.5
@@ -33,14 +48,15 @@ cursor_visible = False
 def render_input():
     global cursor_visible, prev_time
     text = font.render(CD + "> " + key_handler.command_input, False, (255, 255, 255))
-    screen.blit(text, (5, 5 + (font_size * len(history))))
+    if mouse_wheel_offset <= 0:
+        screen.blit(text, (5, 5 + (font_size * len(visible_history))))
     if time.time() - prev_time >= cursor_delay:
         cursor_visible = not cursor_visible
         prev_time = time.time()
-    if cursor_visible:
+    if cursor_visible and mouse_wheel_offset <= 0:
         cd_w, cd_l = font.size(CD + "> " + key_handler.command_input)
-        pygame.draw.line(screen, (255, 255, 255), (cd_w + 5, 5 + (font_size * (len(history) + 1))),
-                         (cd_w + 10, 5 + (font_size * (len(history) + 1))), 1)
+        pygame.draw.line(screen, (255, 255, 255), (cd_w + 5, 5 + (font_size * (len(visible_history) + 1))),
+                         (cd_w + 10, 5 + (font_size * (len(visible_history) + 1))), 1)
 
 
 def process_command(command):
@@ -75,6 +91,8 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             key_handler.tick(event.key)
+        if event.type == pygame.MOUSEWHEEL:
+            mouse_wheel_offset += event.y
     screen.fill((0, 0, 0))
     render_history()
     render_input()
