bb624e7dcc9515265ccf87f0ffc3d8682b01b16b
John <mwxfranco@gmail.com>
2022-05-29 14:18:44 +0100

    added network shares

--- computers.py
+++ computers.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.font.init()
 
+
 class Computer:
     def __init__(self, hostname, ip):
         self.drives = {}
@@ -11,6 +12,7 @@ class Computer:
         self.font_size = 12
         self.font_color = (255, 255, 255)
         self.font = pygame.font.SysFont("Consolas", self.font_size)
+        self.shares = []
 
     def get(self, drive_letter):
         for drive in self.drives:
@@ -30,6 +32,21 @@ class Computer:
         self.drives[drive.letter] = drive
         return self
 
+    def build(self):
+        for letter in self.drives:
+            drive = self.drives[letter]
+            if drive.shared:
+                self.shares.append(Share(letter, letter + ":"))
+            for name in drive.contents:
+                folder = drive.contents[name]
+                self.create_shares(folder, letter + ":/" + name)
+
+    def create_shares(self, folder, path):
+        if folder.shared:
+            self.shares.append(Share(folder.name, path + "/" + folder.name))
+        for f in folder.contents:
+            self.create_shares(folder.contents[f], path + "/" + folder.name)
+
     def path_exists(self, path):
         components = path.split('/')
         components[0] = components[0][0]
@@ -47,9 +64,27 @@ class Computer:
 
 
 class Drive:
-    def __init__(self, letter):
+    def __init__(self, letter, shared=False):
         self.letter = letter
+        self.name = letter
         self.contents = {}
+        self.shared = shared
+
+    def get(self, name):
+        for key in self.contents:
+            if name.lower() == key.lower():
+                return self.contents[key]
+
+    def contains(self, object):
+        self.contents[object.name] = object
+        return self
+
+
+class Folder():
+    def __init__(self, name, shared=False):
+        self.name = name
+        self.contents = {}
+        self.shared = shared
 
     def get(self, name):
         for key in self.contents:
@@ -68,72 +103,65 @@ class File():
         self.contents = ""
 
 
-class Folder():
-    def __init__(self, name):
+class Share:
+    def __init__(self, name, path):
         self.name = name
-        self.contents = {}
-
-    def get(self, name):
-        for key in self.contents:
-            if name.lower() == key.lower():
-                return self.contents[key]
-
-    def contains(self, object):
-        self.contents[object.name] = object
-        return self
+        self.path = path
 
 
 SysAdmin = Computer("SysAdmin", "192.168.1.43")
-SysAdmin.contains(Drive("C")
-                   .contains(Folder("Windows"))
-                   .contains(Folder("Users")
-                             .contains(Folder("Default"))
-                             .contains(Folder("Michael Franco")
-                                       .contains(Folder("Desktop"))
-                                       .contains(Folder("Documents"))
-                                       .contains(Folder("3D Objects"))
-                                       .contains(Folder("Contacts"))
-                                       .contains(Folder("Downloads"))
-                                       .contains(Folder("AppData"))
-                                       .contains(Folder("Favorites"))
-                                       .contains(Folder("Links"))
-                                       .contains(Folder("Favorites"))
-                                       .contains(Folder("Links"))
-                                       .contains(Folder("Music"))
-                                       .contains(Folder("OneDrive"))
-                                       .contains(Folder("Pictures"))
-                                       .contains(Folder("Saved Games"))
-                                       .contains(Folder("Searches"))
-                                       .contains(Folder("Videos"))
-                                       ))
-                   .contains(Folder("Programs"))
-                   .contains(Folder("Programs (x86)"))
-                   )
+SysAdmin.contains(Drive("C", shared=True)
+                  .contains(Folder("Windows"))
+                  .contains(Folder("Users")
+                            .contains(Folder("Default"))
+                            .contains(Folder("Michael Franco")
+                                      .contains(Folder("Desktop"))
+                                      .contains(Folder("Documents"))
+                                      .contains(Folder("3D Objects"))
+                                      .contains(Folder("Contacts"))
+                                      .contains(Folder("Downloads"))
+                                      .contains(Folder("AppData"))
+                                      .contains(Folder("Favorites"))
+                                      .contains(Folder("Links"))
+                                      .contains(Folder("Favorites"))
+                                      .contains(Folder("Links"))
+                                      .contains(Folder("Music"))
+                                      .contains(Folder("OneDrive"))
+                                      .contains(Folder("Pictures"))
+                                      .contains(Folder("Saved Games"))
+                                      .contains(Folder("Searches"))
+                                      .contains(Folder("Videos"))
+                                      ))
+                  .contains(Folder("Programs"))
+                  .contains(Folder("Programs (x86)"))
+                  )
 SysAdmin.contains(Drive("D"))
+SysAdmin.build()
 
 Server = Computer("server", "10.1.1.1")
-Server.contains(Drive("C")
-                   .contains(Folder("Windows"))
-                   .contains(Folder("Users")
-                             .contains(Folder("Default"))
-                             .contains(Folder("Ligma Balls")
-                                       .contains(Folder("Desktop"))
-                                       .contains(Folder("Documents"))
-                                       .contains(Folder("3D Objects"))
-                                       .contains(Folder("Contacts"))
-                                       .contains(Folder("Downloads"))
-                                       .contains(Folder("AppData"))
-                                       .contains(Folder("Favorites"))
-                                       .contains(Folder("Links"))
-                                       .contains(Folder("Favorites"))
-                                       .contains(Folder("Links"))
-                                       .contains(Folder("Music"))
-                                       .contains(Folder("OneDrive"))
-                                       .contains(Folder("Pictures"))
-                                       .contains(Folder("Saved Games"))
-                                       .contains(Folder("Searches"))
-                                       .contains(Folder("Videos"))
-                                       ))
-                   .contains(Folder("Programs"))
-                   .contains(Folder("Programs (x86)"))
-                   )
+Server.contains(Drive("C", shared=True)
+                .contains(Folder("Windows"))
+                .contains(Folder("Users")
+                          .contains(Folder("Default"))
+                          .contains(Folder("Ligma Balls")
+                                    .contains(Folder("Desktop", shared=True))
+                                    .contains(Folder("Documents", shared=True))
+                                    .contains(Folder("3D Objects", shared=True))
+                                    .contains(Folder("Contacts", shared=True))
+                                    .contains(Folder("Downloads", shared=True))
+                                    .contains(Folder("AppData"))
+                                    .contains(Folder("Favorites"))
+                                    .contains(Folder("Links"))
+                                    .contains(Folder("Favorites"))
+                                    .contains(Folder("Links"))
+                                    .contains(Folder("Music"))
+                                    .contains(Folder("OneDrive"))
+                                    .contains(Folder("Pictures"))
+                                    .contains(Folder("Saved Games"))
+                                    .contains(Folder("Searches"))
+                                    .contains(Folder("Videos"))
+                                    ))
+                .contains(Folder("Programs"))
+                .contains(Folder("Programs (x86)"))
+                )
+Server.build()
